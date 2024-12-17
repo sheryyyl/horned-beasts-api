bddbad0239f40c15d0aa5bacfab6bd44c18c2031
Sheryl Condori <condorisheryl@gmail.com>
2024-12-17 10:19:56 -0500

    lab07

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 import express from "express"
 import 'dotenv/config'
+import beastsRouter from "./routes/beasts.js"
+import morgan from "morgan"
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000;
+app.use(morgan("tiny"))
+app.use("/api", beastsRouter)
 
-app.get('/', (req, res) => {
-  res.send('Horned Beasts API de Sheryl');
-});
-app.get('/test', (req, res) => {
-  res.json({
-    "name": "[TuNombre]",
-    "date": "2024-10-28",
-    "timestamp": 1730258872
-  })
-});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
